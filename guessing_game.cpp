0b12a9851654f256a929257a64fb7df2dbbea9c8
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-08-02 09:24:23 +0200

    finished first part

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -1,21 +1,76 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void numbersGenerator();
+int SecretnumberGenerator(int num);
+bool gameProcessor();    //function that control game process and determine if the user won.
 
 int main(){
 
-    cout << "Hello world lets make a guessing game!" << endl;
+    cout << "=== Welcome to the game ===" << endl;
+    cout << endl;
+
+    gameProcessor();
+
     return 0;
 
 }
 
-void numbersGenerator() {
-    int i, arr_number[100];
+int SecretnumberGenerator(int num) {
 
-    for(i=0; i<=100; i++) {
-        arr_number[i] = i;
+    int i, secretNumber[100];
+
+    for (i = 200; i > 100; i--) {
+        secretNumber[i] = i; 
     }
 
+    switch(num) {
+        case 1: 
+            return secretNumber[72];
+            break;
+        case 2:
+            return secretNumber[10];
+            break;
+        case 3:
+            return secretNumber[89];
+            break;
+        case 4: 
+            return secretNumber[31];
+            break;
+        case 5:
+            return secretNumber[56];
+            break;
+        default:
+            break;
+    }
+
+    return secretNumber[32];
+
+}
+
+bool gameProcessor(){
+
+    int userGuess, count;
+    int secretNumber;
+
+    do{
+
+        secretNumber = SecretnumberGenerator(count);
+
+        cout << "Enter your guess : ";
+        cin >> userGuess;
+
+        if(userGuess == secretNumber) {
+            cout << "***************" << "You win !!!" << "***************" << endl;
+        }
+
+        count++;
+
+        if(count > 5) {
+            cout << "You are out of guess times";
+        }
+
+    } while (userGuess != secretNumber);
+    
 }
